68fa7e0f5154f51637ce0df2c51eecc8456f3e19
quou <georgeashleylamb@gmail.com>
2022-11-03 10:29:45 +1100

    Update the Visual Studio projects to improve the installer.

---
+++ projects/vs2022/buildall/buildall.bat
@@ -0,0 +1,2 @@
+msbuild %1 /p:Configuration=DebugCompile
+msbuild %1 /p:Configuration=ReleaseCompile
--- projects/vs2022/install/install.bat
+++ projects/vs2022/install/install.bat
@@ -1,6 +1,7 @@
 :: Installs corrosion development files to C:\Program Files\corrosion.
 :: Requires the solution directory to be passed as the first argument
-:: and the configuration to be passed as the second.
+:: and the debug configuration name to be passed as the second, with
+:: the third argument being the release configuration name.
 
 set install_dir=C:\Program Files\corrosion\
 
@@ -11,8 +12,9 @@ md "%install_dir%lib"
 echo %1..\..\corrosion\include\
 
 xcopy /e /s /y /i "%1..\..\corrosion\include\*" "%install_dir%include"
-xcopy /e /s /y /i "%1corrosion\bin\%2\corrosion.lib" "%install_dir%lib"
-copy  /y "%1shadercompiler\bin\%2\shadercompiler.exe" "%install_dir%bin\cshc.exe"
+copy /y "%1corrosion\bin\%2\corrosion.lib" "%install_dir%lib\corrosiond.lib"
+copy /y "%1corrosion\bin\%3\corrosion.lib" "%install_dir%lib\corrosion.lib"
+copy  /y "%1shadercompiler\bin\%3\shadercompiler.exe" "%install_dir%bin\cshc.exe"
 
 setx /m CORROSION_SDK "%install_dir%
 setx /m CORROSION_DEPS "vulkan-1.lib;opengl32.lib;winmm.lib;
